e3200e64338dd2708ef25f3378e830306b03854d
iaberumen <iaberumen@github.com>
2021-05-14 16:23:50 -0600

    correccion en el modelo

--- employees/models.py
+++ employees/models.py
@@ -4,9 +4,16 @@ from django.db import models
 class Employee(models.Model):
     curp = models.CharField(max_length=20,blank=True)
     rfc = models.CharField(max_length=20,blank=False)
-    fecha_nacimiento = models.DateTimeField(blank=False)
-    fecha_accidente = models.DateTimeField(blank=True)
+    fecha_nacimiento = models.DateField(blank=False)
+    fecha_accidente = models.DateField(null=True,blank=True,default=None)
 
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
+    
+        constraints = [
+                models.UniqueConstraint(fields=['rfc'], name='')
+            ]
+    
+    def __str__(self):
+        return self.name
